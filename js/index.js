3a5438ec0b973a74646326ca07ec8c633dfb6a1b
AlenV <65910280+alenvalincic@users.noreply.github.com>
2021-01-23 23:42:06 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("btn");
+const content = document.getElementById("content");
+
+content.style.display = "none";
+
+button.addEventListener("click", (Event)  =>{
+    if(content.style.display == "none"){
+        content.style.display = "block"
+    } else(content.style.display = "none")
+
+})
+
